cf466fff5a86e11346cca59c1f7a4282dffa34e4
virsys <virsys.electron@gmail.com>
2020-01-04 00:18:51 +0100

    New syscall and removal of ccache Google's build rules disallow the use of ccache and are enforced as of 1/1/2020. A new syscall has been introduced to our kernel, too.

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -705,6 +705,8 @@ __SYSCALL(__NR_renameat2, sys_renameat2)
 __SYSCALL(__NR_seccomp, sys_seccomp)
 #define __NR_getrandom 278
 __SYSCALL(__NR_getrandom, sys_getrandom)
+#define __NR_memfd_create 279
+__SYSCALL(__NR_memfd_create, sys_memfd_create)
 
 #undef __NR_syscalls
 #define __NR_syscalls 279
